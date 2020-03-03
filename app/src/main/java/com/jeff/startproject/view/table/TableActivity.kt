ac81edcc7f76dafbf6e3b4bc61fd85051a659dfb
Jeff.Yu <jeffyu925@gmail.com>
2020-03-03 18:26:31 +0800

    RecyclerView + CollapsingToolbarLayout + fitsSystemWindows

--- app/src/main/java/com/jeff/startproject/view/table/TableActivity.kt
+++ app/src/main/java/com/jeff/startproject/view/table/TableActivity.kt
@@ -1,9 +1,12 @@
 package com.jeff.startproject.view.table
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.jeff.startproject.R
 import com.jeff.startproject.databinding.ActivityTableBinding
 import com.jeff.startproject.view.base.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -18,6 +21,22 @@ class TableActivity : BaseActivity<ActivityTableBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.statusBarColor = Color.TRANSPARENT
+
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_share -> {
+                    Toast.makeText(this, R.string.text_table_recycler_view, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
 
         val userInfoListAdapter = UserInfoListAdapter()
 
