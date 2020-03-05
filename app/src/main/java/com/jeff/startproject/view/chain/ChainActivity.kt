4e61a5413f5e0a67760796f14614150edfbf36d2
Jeff.Yu <jeffyu925@gmail.com>
2020-03-05 13:12:04 +0800

    Add Chain sample

---
+++ app/src/main/java/com/jeff/startproject/view/chain/ChainActivity.kt
@@ -0,0 +1,14 @@
+package com.jeff.startproject.view.chain
+
+import com.jeff.startproject.databinding.ActivityChainBinding
+import com.jeff.startproject.view.base.BaseActivity
+
+/*
+ * https://www.jowanxu.top/2018/01/15/Android-ConstraintLayout-Chains
+ */
+
+class ChainActivity : BaseActivity<ActivityChainBinding>() {
+    override fun getViewBinding(): ActivityChainBinding {
+        return ActivityChainBinding.inflate(layoutInflater)
+    }
+}
