097798c44e1fdb12f49eaad5d6a53cc51879d0e6
Jeff.Yu <jeffyu925@gmail.com>
2020-03-12 14:56:15 +0800

    Updated status bar color when connect change.

--- app/src/main/java/com/jeff/startproject/view/websocket/WebSocketActivity.kt
+++ app/src/main/java/com/jeff/startproject/view/websocket/WebSocketActivity.kt
@@ -3,19 +3,13 @@ package com.jeff.startproject.view.websocket
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.jeff.startproject.BuildConfig
+import com.jeff.startproject.R
 import com.jeff.startproject.databinding.ActivityWebsocketBinding
-import com.jeff.startproject.utils.socket.JFWebSocketClient
-import com.jeff.startproject.utils.socket.JFWebSocketListener
 import com.jeff.startproject.view.base.BaseActivity
-import com.log.JFLog
-import org.java_websocket.handshake.ServerHandshake
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.lang.Exception
-import java.net.URI
 
 class WebSocketActivity : BaseActivity<ActivityWebsocketBinding>() {
-    override fun getViewBinding(): ActivityWebsocketBinding =
-        ActivityWebsocketBinding.inflate(layoutInflater)
+    override fun getViewBinding(): ActivityWebsocketBinding = ActivityWebsocketBinding.inflate(layoutInflater)
 
     private val viewModel by viewModel<WebSocketViewModel>()
 
@@ -26,46 +20,17 @@ class WebSocketActivity : BaseActivity<ActivityWebsocketBinding>() {
             binding.textLog.text = message
         })
 
-        if (BuildConfig.DEBUG) {
-            val listener = object : JFWebSocketListener {
-                override fun onOpen(handshake: ServerHandshake) {
-                    JFLog.d("onOpen status : ${handshake.httpStatus}").also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                    JFLog.d("onOpen message: ${handshake.httpStatusMessage}").also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                }
-
-                override fun onMessage(message: String) {
-                    JFLog.d("onMessage: $message").also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                }
-
-                override fun onClose(code: Int, reason: String?, remote: Boolean) {
-                    JFLog.d("onClose code: $code").also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                    JFLog.d("onClose reason: $reason").also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                    JFLog.d("onClose remote: $remote").also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                }
-
-                override fun onError(e: Exception) {
-                    JFLog.e(e).also {
-                        viewModel.appendMessage(it, isPost = true)
-                    }
-                }
+        viewModel.isConnection.observe(this, Observer {
+            window?.statusBarColor = when (it) {
+                true -> R.color.pale_green
+                false -> R.color.orange_red
+            }.let { res ->
+                resources.getColor(res, null)
             }
+        })
 
-            val uri = URI.create("ws://${BuildConfig.LOCAL_IP}:8080/ws/client")
-            val socketClient = JFWebSocketClient(uri)
-            socketClient.listener = listener
-            socketClient.connect()
+        if (BuildConfig.DEBUG) {
+            viewModel.connectLocal()
         }
     }
 }
--- app/src/main/java/com/jeff/startproject/view/websocket/WebSocketViewModel.kt
+++ app/src/main/java/com/jeff/startproject/view/websocket/WebSocketViewModel.kt
@@ -1,6 +1,65 @@
 package com.jeff.startproject.view.websocket
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.jeff.startproject.BuildConfig
+import com.jeff.startproject.utils.socket.JFWebSocketClient
+import com.jeff.startproject.utils.socket.JFWebSocketListener
 import com.jeff.startproject.view.base.LogcatViewModel
+import com.log.JFLog
+import org.java_websocket.handshake.ServerHandshake
+import java.net.URI
 
 class WebSocketViewModel : LogcatViewModel() {
+
+    private val mIsConnection = MutableLiveData(false)
+    val isConnection: LiveData<Boolean> = mIsConnection
+
+    fun connectLocal() {
+        val listener = object : JFWebSocketListener {
+            override fun onOpen(handshake: ServerHandshake) {
+                mIsConnection.postValue(true)
+
+                JFLog.d("onOpen status : ${handshake.httpStatus}").also {
+                    appendMessage(it, isPost = true)
+                }
+                JFLog.d("onOpen message: ${handshake.httpStatusMessage}").also {
+                    appendMessage(it, isPost = true)
+                }
+            }
+
+            override fun onMessage(message: String) {
+                JFLog.d("onMessage: $message").also {
+                    appendMessage(it, isPost = true)
+                }
+            }
+
+            override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                mIsConnection.postValue(false)
+
+                JFLog.d("onClose code: $code").also {
+                    appendMessage(it, isPost = true)
+                }
+                JFLog.d("onClose reason: $reason").also {
+                    appendMessage(it, isPost = true)
+                }
+                JFLog.d("onClose remote: $remote").also {
+                    appendMessage(it, isPost = true)
+                }
+            }
+
+            override fun onError(e: Exception) {
+                mIsConnection.postValue(false)
+
+                JFLog.e(e).also {
+                    appendMessage(it, isPost = true)
+                }
+            }
+        }
+
+        val uri = URI.create("ws://${BuildConfig.LOCAL_IP}:8080/ws/client")
+        val socketClient = JFWebSocketClient(uri)
+        socketClient.listener = listener
+        socketClient.connect()
+    }
 }
