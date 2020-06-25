8cedd43d75a8e9b48001ea6d758870f1543d5c60
Jeff.Yu <jeffyu925@gmail.com>
2020-06-25 17:27:10 +0800

    Upgrade Gradle & Libs

--- app/src/main/java/com/jeff/startproject/view/navigation/NavigationActivity.kt
+++ app/src/main/java/com/jeff/startproject/view/navigation/NavigationActivity.kt
@@ -1,5 +1,7 @@
 package com.jeff.startproject.view.navigation
 
+import android.os.Bundle
+import com.jeff.startproject.R
 import com.jeff.startproject.databinding.ActivityNavigationBinding
 import com.view.base.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -13,4 +15,25 @@ class NavigationActivity: BaseActivity<ActivityNavigationBinding>() {
     override fun getViewBinding():ActivityNavigationBinding = ActivityNavigationBinding.inflate(layoutInflater)
 
     private val viewModel by viewModel<NavigationViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(R.navigation.nav_file)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        binding.bottomNav.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+    }
 }
