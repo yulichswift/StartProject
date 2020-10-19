85332f4d10fdc31fa6e7028f765a8599bfb5c750
Jeff.Yu <jeffyu925@gmail.com>
2020-10-19 16:11:15 +0800

    1. Shortcut 2. Use diff taskAffinity to open diff activity by shortcut

--- app/src/main/java/com/jeff/startproject/MyApplication.kt
+++ app/src/main/java/com/jeff/startproject/MyApplication.kt
@@ -1,9 +1,15 @@
 package com.jeff.startproject
 
 import android.app.Application
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.os.Build
 import com.jeff.startproject.di.apiModule
 import com.jeff.startproject.di.appModule
 import com.jeff.startproject.di.dbModule
+import com.jeff.startproject.view.blur.BlurActivity
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -29,5 +35,40 @@ class MyApplication : Application() {
             androidContext(this@MyApplication)
             modules(listOf(appModule, dbModule, apiModule))
         }
+
+        addDynamicShortcut(this, "com.jeff.blur")
+    }
+
+    private fun addDynamicShortcut(application: Application, shortcutId: String) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
+            return
+        }
+
+        val shortcutManager = application.getSystemService(ShortcutManager::class.java)
+
+        if (shortcutManager.dynamicShortcuts.count() + shortcutManager.manifestShortcuts.count() > shortcutManager.maxShortcutCountPerActivity) {
+            return
+        }
+
+        val mainIntent = Intent(Intent.ACTION_MAIN, null)
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        mainIntent.setPackage(application.packageName)
+        val activities = application.packageManager.queryIntentActivities(mainIntent, 0)
+        if (activities.isEmpty()) {
+            return
+        }
+
+        val intent = Intent(this, BlurActivity::class.java)
+        intent.action = Intent.ACTION_VIEW
+
+        val shortcut = ShortcutInfo.Builder(application, shortcutId)
+            .setShortLabel("Blur!")
+            .setLongLabel("BlurActivity")
+            .setIcon(Icon.createWithResource(this, R.mipmap.ic_launcher))
+            .setIntent(intent)
+            .build()
+
+        //shortcutManager.dynamicShortcuts = listOf(shortcut)
+        shortcutManager.addDynamicShortcuts(listOf(shortcut))
     }
 }
