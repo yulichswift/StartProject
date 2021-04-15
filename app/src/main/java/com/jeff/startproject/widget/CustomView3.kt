e96b7bf1afd4c9be5aca1002edef13b8193d373a
Jeff.Yu <jeffyu925@gmail.com>
2021-04-15 15:15:52 +0800

    GC log

--- app/src/main/java/com/jeff/startproject/widget/CustomView3.kt
+++ app/src/main/java/com/jeff/startproject/widget/CustomView3.kt
@@ -8,28 +8,37 @@ import com.jeff.startproject.databinding.View3Binding
 import com.log.JFLog
 
 class CustomView3 @kotlin.jvm.JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) :
-    FrameLayout(context, attrs, defStyleAttr) {
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    companion object {
+        var inCacheCount = 0
+    }
 
     private val binding = View3Binding.inflate(LayoutInflater.from(context), this, true)
     val btn by lazy(binding::btn)
+    private val index: Int = inCacheCount
+
+    init {
+        inCacheCount += 1
+    }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
-        JFLog.d("onAttachedToWindow")
+        // JFLog.d("onAttachedToWindow")
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
 
-        JFLog.d("onDetachedFromWindow")
+        // JFLog.d("onDetachedFromWindow")
     }
 
     protected fun finalize() {
-        JFLog.d("GC $this")
+        inCacheCount -= 1
+        JFLog.d("GC $index cache: $inCacheCount")
     }
 }
